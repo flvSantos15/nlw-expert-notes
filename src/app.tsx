b4fb0395865a72fd3ee1173a7fb7303c0a276dd1
flvSantos15 <flvSantos300@gmail.com>
2024-02-06 23:31:07 -0300

    chore: add new note and note component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,11 @@
 import logo from './assets/logo-nlw-experts.svg'
+import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
+
+const note = {
+  date: new Date(),
+  content: 'Hello world'
+}
 
 export function App() {
   return (
@@ -17,20 +23,9 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="tetx-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
+        <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   )
